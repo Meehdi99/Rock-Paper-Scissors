f2fb7f292fe5cebe8aa7244e3170cc0ceafe8285
Meehdi99 <mhalouardi@gmail.com>
2023-01-28 15:44:12 +0100

    Edit functions

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
-let computerSelection = ['rock', 'paper','scissors'];
+let computerSelection = ["rock", "paper", "scissors"];
 let playerSelection;
-let l=0;
-let w=0;
+let l = 0;
+let w = 0;
+let i = 1;
 
-
-function getComputerChoice (){
-    return computerSelection[Math.floor(Math.random()*computerSelection.length)];
+function getComputerChoice() {
+  return Math.floor(Math.random() * computerSelection.length);
 }
 
-
-function playRound(playerSelection,computerSelection){
-    computerSelection = getComputerChoice();
-    alert("Computer chose : "+ computerSelection);
-    if (playerSelection == computerSelection){
-        alert("Tied!");
-    }
-    else if (playerSelection=='rock' && computerSelection=='scissors' || playerSelection =='paper' && computerSelection =='rock' || playerSelection =='scissors' && computerSelection =='paper'){
-        w++;
-        alert("You win! "+playerSelection+" beats "+computerSelection+".");
-    }
-    else{
-        l++;
-        alert("You lose!"+computerSelection+" beats "+playerSelection+".");
-    }
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "Round : " + i + "/5" + "\nTie!" + "\n Score : " + l + " - " + w;
+  } else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    w++;
+    return (
+      "Round : " +
+      i +
+      "/5" +
+      "\nYou win! " +
+      playerSelection +
+      " beats " +
+      computerSelection +
+      "." +
+      "\n Score : " +
+      l +
+      " - " +
+      w
+    );
+  } else {
+    l++;
+    return (
+      "Round : " +
+      i +
+      "/5" +
+      "\nYou lose! " +
+      computerSelection +
+      " beats " +
+      playerSelection +
+      "." +
+      "\n Score : " +
+      l +
+      " - " +
+      w
+    );
+  }
 }
 
-
-function game(){
-    for (let i = 0; i<5; i++) {
-        playerSelection = prompt("Choose between: Paper, Rock or Scissors","Rock");
-        console.log(playRound(playerSelection.toLowerCase(),""));
-    }
-    if (l>w){
-        window.alert("Computer Wins." );
-    }
-    else if (w>l){
-        window.alert("Player Wins.")
-    }
-    else {
-        window.alert("it's a tie.");
-    }
-    l=w=0;
-}    
+function game() {
+  for (i = 1; i < 6; i++) {
+    let c = computerSelection[getComputerChoice()];
+    playerSelection = prompt("Choose between: Paper, Rock or Scissors", "Rock");
+    console.log("Computer chose : " + c);
+    console.log(playRound(playerSelection.toLowerCase(), c));
+  }
+  if (l > w) {
+    console.log("Computer Wins!");
+  } else if (w > l) {
+    console.log("You Win!");
+  } else {
+    console.log("it's a tie!");
+  }
+  l = w = 0;
+}
