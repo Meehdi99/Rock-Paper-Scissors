62dbeda35bcde676d779feb80daeac7f12413054
Meehdi99 <mhalouardi@gmail.com>
2023-01-28 18:41:50 +0100

    Add br

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let computerSelection = ["Rock", "Paper", "Scissors"];
 let playerSelection;
 let l = 0,
-  w = 0,
-  i = 1;
+  w = 0;
 
 function getComputerChoice() {
   return Math.floor(Math.random() * computerSelection.length);
@@ -10,7 +9,14 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return "\n It's a tie!" + "\n Score : Computer : " + l + " - Player : " + w;
+    return (
+      "<br> It's a tie! You both chose " +
+      playerSelection +
+      "<br> Score : Computer : " +
+      l +
+      " - Player : " +
+      w
+    );
   } else if (
     (playerSelection == "Rock" && computerSelection == "Scissors") ||
     (playerSelection == "Paper" && computerSelection == "Rock") ||
@@ -18,12 +24,12 @@ function playRound(playerSelection, computerSelection) {
   ) {
     w++;
     return (
-      "\n You win! " +
+      "<br> You win! " +
       playerSelection +
       " beats " +
       computerSelection +
       "." +
-      "\n Score : Computer : " +
+      "<br> Score : Computer : " +
       l +
       " - Player : " +
       w
@@ -31,24 +37,24 @@ function playRound(playerSelection, computerSelection) {
   } else {
     l++;
     return (
-      "\n You lose! " +
+      "<br> You lose! " +
       computerSelection +
       " beats " +
       playerSelection +
       "." +
-      "\n Score : Computer : " +
+      "<br> Score : Computer : " +
       l +
       " - Player : " +
       w
     );
   }
 }
+
 const body = document.querySelector("body");
 const results = document.createElement("div");
 results.classList.add("results");
 
 const buttons = document.querySelectorAll("button");
-
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     game(button.id);
@@ -59,15 +65,12 @@ function game(playerSelection) {
   let c = computerSelection[getComputerChoice()];
   if (w == 5) {
     results.textContent = "You win!";
-    body.appendChild(results);
     l = w = 0;
   } else if (l == 5) {
     results.textContent = "Computer wins!";
-    body.appendChild(results);
     l = w = 0;
   } else {
-    results.textContent =
-      "Computer chose : " + c + playRound(playerSelection, c);
-    body.appendChild(results);
+    results.innerHTML = "Computer chose : " + c + playRound(playerSelection, c);
   }
+  body.appendChild(results);
 }
